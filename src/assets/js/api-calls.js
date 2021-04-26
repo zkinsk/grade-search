4110fe71a55edff9680bcc82926dc4cd96cd4f6b
Scott Zinski <zoolunk@gmail.com>
2021-04-26 18:26:54 -0400

    mock api calls

--- src/assets/js/api-calls.js
+++ src/assets/js/api-calls.js
@@ -1,9 +1,24 @@
+import { me } from '../../hide/me';
+
 const rootUrl = `https://bootcampspot.com`;
+
+const mockLogin = {
+  success: true,
+  errorCode: null,
+  resetToken: null,
+  authenticationInfo: {
+    userId: 13786,
+    firstLogin: false,
+    active: true,
+    authToken: 'fake-token',
+  },
+};
 
 export const getToken = ({ email, password }) => {
   if (!email || !password) {
     throw new Error('Complete Login form');
   }
+  return Promise.resolve(mockLogin);
   const url = rooUrl + '/api/instructor/v1/login';
   return $.ajax({
     contentType: 'application/json',
@@ -38,6 +53,7 @@ export const getMe = (authToken) => {
   if (!authToken) {
     throw new Error('No Auth Token Provided');
   }
+  return Promise.resolve(me);
   const url = rootUrl + '/api/instructor/v1/me';
   return $.ajax({
     url: url,
