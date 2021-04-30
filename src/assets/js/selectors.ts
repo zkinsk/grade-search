65ecc22f4ff871e2be5168a870c3c1b4f833e565
Scott Zinski <zoolunk@gmail.com>
2021-04-30 18:59:55 -0400

    set selector types

--- src/assets/js/selectors.ts
+++ src/assets/js/selectors.ts
@@ -1,14 +1,14 @@
 import jquery from 'jquery';
 const $ = (window.$ = window.jQuery = jquery);
 
-export const loginForm = $('.login-form');
-export const inputs = $('.login-form input');
-export const loginFormContainer = $('.login-form-container');
-export const getGradesBtn = $('.get-grades');
-export const assignmentRootElem = $('.assignment-cards');
-export const assignmentButtonContainer = $('.assignment-buttons');
-export const logoutButtonElem = $('.logout-button');
-export const alertDangerElem = $('.alert-danger');
-export const alertInfoElem = $('.alert-info');
+export const loginForm: JQuery<HTMLFormElement> = $('.login-form');
+export const inputs: JQuery<HTMLInputElement> = $('.login-form input');
+export const loginFormContainer: JQuery<HTMLFormElement> = $('.login-form-container');
+export const getGradesBtn: JQuery<HTMLButtonElement> = $('.get-grades');
+export const assignmentRootElem: JQuery<HTMLDivElement> = $('.assignment-cards');
+export const assignmentButtonContainer: JQuery<HTMLDivElement> = $('.assignment-buttons');
+export const logoutButtonElem: JQuery<HTMLButtonElement> = $('.logout-button');
+export const alertDangerElem: JQuery<HTMLDivElement> = $('.alert-danger');
+export const alertInfoElem: JQuery<HTMLDivElement> = $('.alert-info');
 
 export default $;
