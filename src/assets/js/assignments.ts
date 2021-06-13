3b7e5e52877cfa77378a571406b994b2a2375c0d
Scott Zinski <zoolunk@gmail.com>
2021-06-13 12:29:20 -0400

    remove junk

--- src/assets/js/assignments.ts
+++ src/assets/js/assignments.ts
@@ -1,8 +1,6 @@
 import { sortedAssignments } from '../data/sorted-assignments';
-import { tableRow } from '../components/class-card-table-row';
-import { assignmentCard } from '../components/class-cards';
 
-import Grades, { SortedAssignment, AdaptedGrades, MappedStudentsWithAssignments } from './types/grades';
+import Grades, { MappedStudentsWithAssignments } from './types/grades';
 import StudentAssignmentGrade from './types/grades';
 import CohortAssignments, { MappedAssignments } from './types/calendar-assignments';
 
@@ -48,21 +46,6 @@ export const buildCurrentCalendarAssignmentList = (data: CohortAssignments) => {
     if (item.category.code === 'career' || new Date(item.assignmentDate) > now) return acc;
     return [...acc, item.title];
   }, []);
-};
-
-const buildTableRows = (grades: AdaptedGrades[]) => {
-  return grades.map((grade) => tableRow(grade)).join('');
-};
-
-export const buildAssignmentCards = (assignmentRoot: JQuery, data: SortedAssignment[]) => {
-  data.forEach(({ assignmentTitle: title, grades }) => {
-    // console.log('Grades: ', grades);
-    const card = assignmentCard({
-      title: title,
-      grades: buildTableRows(grades),
-    });
-    assignmentRoot.append(card);
-  });
 };
 
 export const reduceCohortAssignments = (data: CohortAssignments): MappedAssignments => {
