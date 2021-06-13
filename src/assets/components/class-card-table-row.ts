b413381ae9239bf7a288127407e22a04b7d44cfc
Scott Zinski <zoolunk@gmail.com>
2021-06-13 12:25:51 -0400

    build out adapter & temporary page update

--- src/assets/components/class-card-table-row.ts
+++ src/assets/components/class-card-table-row.ts
@@ -4,7 +4,7 @@ type TableRow = (arg0: AdaptedGrades) => string;
 
 export const tableRow: TableRow = ({ submitted, studentName, grade }) => {
   return /*html*/ `<tr class=${submitted ? '' : 'not-submitted'}>
-  <td>${studentName}</td>
+  <td data-student-name="${studentName.toLowerCase()}">${studentName}</td>
   <td>${grade}</td>
 </tr>
   `;
--- src/assets/js/attendance/attendance.ts
+++ src/assets/js/attendance/attendance.ts
@@ -4,7 +4,6 @@ import { getAttendanceButton } from '../selectors';
 
 const useGetAttendance = (authObj: { token: null | string }, courseId = 3020) => {
   getAttendanceButton.on('click', async function () {
-    console.log('Button clicked');
     const res = await getAttendance(courseId, authObj.token);
     const filteredResults = res.filter(
       ({ pending, sessionName }) =>
@@ -12,14 +11,38 @@ const useGetAttendance = (authObj: { token: null | string }, courseId = 3020) =>
           pending ||
           sessionName.toLowerCase().includes('not required') ||
           sessionName.toLowerCase().includes('orientation event') ||
-          sessionName.toLowerCase().includes('Career Paths')
+          sessionName.toLowerCase().includes('career paths')
         )
     );
-    const sessions = new Set();
-    filteredResults.forEach(({ sessionName }) => {
-      sessions.add(sessionName);
+    const studentAttendance = new Map();
+
+    filteredResults.forEach(({ studentName, present, excused }) => {
+      const cs = studentAttendance.get(studentName);
+      if (!cs) {
+        studentAttendance.set(studentName, {
+          sessions: 1,
+          absent: present ? 0 : 1,
+          excused: excused !== null && excused === false ? 1 : 0,
+          unexcused: excused ? 1 : 0,
+        });
+        return;
+      }
+      studentAttendance.set(studentName, {
+        sessions: cs.sessions + 1,
+        absent: present ? cs.absent : cs.absent + 1,
+        unexcused: excused !== null && excused === false ? cs.unexcused + 1 : cs.unexcused,
+        excused: excused ? cs.excused + 1 : cs.excused,
+      });
     });
-    console.log('Res: ', sessions);
+
+    console.log('map: ', studentAttendance);
+
+    for (const [name, obj] of studentAttendance) {
+      const span = document.createElement('span');
+      const hr = document.createElement('div');
+      span.innerText = ` A:${obj.absent} / U:${obj.unexcused} / T:${obj.sessions}`;
+      document.querySelector(`[data-student-name="${name.toLowerCase()}"]`)?.append(hr, span);
+    }
   });
 
   return {};
